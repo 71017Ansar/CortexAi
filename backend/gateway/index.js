42c5d63b6215b383bdae02ab2e07a8706fbe5ea1
71017Ansar <71017@gcslahore.edu.pk>
2026-07-10 01:47:05 +0500

    route configure

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -5,6 +5,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({ message: "Gateway server is running" });
+});
+
 app.listen(PORT, () => {
   console.log(`Gateway server is running on port ${PORT}`);
 })
